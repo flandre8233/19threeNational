8ee216e2fe4bd6865c8c0964e8bb74adc7819320
flandre8233 <flandre8233@gmail.com>
2017-06-05 14:44:21 +0800

    alpha 2.6.3

--- Assets/bagManager.cs
+++ Assets/bagManager.cs
@@ -34,23 +34,26 @@ public class bagManager : MonoBehaviour {
             for (int J = 0; J < (boardWidthNumber); J++) {
                 Debug.Log(i + "/" + J);
                 boardData item = new boardData();
-                item.position = new Vector2(J * (boardSize.x / boardWidthNumber) + (int)((boardStartPoint.x / 100) * Screen.width), -i * (boardSize.y / boardHeightNumber) - (int)((boardStartPoint.y / 100) * Screen.height));
+                //item.position = new Vector2(J * (boardSize.x / boardWidthNumber) + (int)((boardStartPoint.x / 100) * Screen.width), -i * (boardSize.y / boardHeightNumber) - (int)((boardStartPoint.y / 100) * Screen.height));
+                item.position = new Vector2(J * (boardSize.x / boardWidthNumber) + (int)((boardStartPoint.x / 100) * Screen.width), -i * (boardSize.x / boardWidthNumber) - (int)((boardStartPoint.y / 100) * Screen.height));
                 item.positionCenter = item.position;
                 item.positionCenter.x += (boardSize.x / boardWidthNumber) / 2;
+                //item.positionCenter.y -= (boardSize.y / boardHeightNumber) / 2;
                 item.positionCenter.y -= (boardSize.y / boardHeightNumber) / 2;
+
                 item.index = new Vector2(J, i);
                 bagDataArray[J, i] = item;
 
             }
         }
         //cardSmallPicSize = new Vector2((boardSize.x / boardWidthNumber) - 20, (boardSize.y / boardHeightNumber) - 20 );
-        cardSmallPicSize = new Vector2((boardSize.x / boardWidthNumber) - 40, (boardSize.x / boardHeightNumber) - 40 );
+        cardSmallPicSize = new Vector2((boardSize.x / boardWidthNumber) - 40, (boardSize.x / boardWidthNumber) - 40 );
     }
 
 
     
     void serializeBag() {
-        //boardSizeNumber.y = Mathf.RoundToInt(playerData.Static.playerCardData.Count / boardSizeNumber.x)+1; //<----
+        boardSizeNumber.y = Mathf.RoundToInt(playerData.Static.playerCardData.Count / boardSizeNumber.x) + 1  ; //<----
         setBoard((int)boardSizeNumber.x, (int)boardSizeNumber.y);
         List<boardData> totalEmptyGridIndex = getTotalEmptyGridIndex(bagDataArray);
 
@@ -76,8 +79,8 @@ public class bagManager : MonoBehaviour {
 
     public List<boardData> getTotalEmptyGridIndex(boardData[,] boardDataArray) {
         List<boardData> totalEmptyGridIndex = new List<boardData>();
-        for (int i = 0; i < ((int)boardSizeNumber.x); i++) {
-            for (int j = 0; j < ((int)boardSizeNumber.y); j++) {
+        for (int i = 0; i < ((int)boardSizeNumber.y); i++) {
+            for (int j = 0; j < ((int)boardSizeNumber.x); j++) {
                 if (boardDataArray[j,i].haveBead == null) {
                     totalEmptyGridIndex.Add(boardDataArray[j,i] );
                 }
@@ -95,9 +98,9 @@ public class bagManager : MonoBehaviour {
             Static = this;
         }
         screenSize = new Vector2(Screen.width, Screen.height);
-        boardSize = new Vector2(Screen.width, Screen.height * 0.5f);
+        //boardSize = new Vector2(Screen.width, Screen.height * 0.5f);
+        boardSize = new Vector2(Screen.width, 4000 );
         serializeBag();
-
 
 
 
---
+++ Assets/selectLevelButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectLevelButtonManager : MonoBehaviour {
+
+    void goLevel(int selectLevelIndex)
+    {
+
+    }
+
+}
